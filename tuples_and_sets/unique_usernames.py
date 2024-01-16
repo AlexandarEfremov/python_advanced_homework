1fd84cd433bd496748d78d1b0c684f77018f5bb5
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-01-16 06:16:26 +0200

    16.01.24

--- tuples_and_sets/unique_usernames.py
+++ tuples_and_sets/unique_usernames.py
@@ -1,9 +1,11 @@
-usernames_num = int(input())
-username_set = set()
+# usernames_num = int(input())
+# username_set = set()
+#
+# for i in range(usernames_num):
+#     name = input()
+#     username_set.add(name)
+# for nam in username_set:
+#     print(nam)
 
-for i in range(usernames_num):
-    name = input()
-    username_set.add(name)
-for nam in username_set:
-    print(nam)
-
+username_set = {input() for _ in range(int(input()))}
+print(*username_set, sep="\n")
