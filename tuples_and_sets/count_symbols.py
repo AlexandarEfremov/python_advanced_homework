6f6ea0c70fcdba0fcde31015757355e5efb2b65c
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-01-16 07:21:21 +0200

    16.01.24

--- tuples_and_sets/count_symbols.py
+++ tuples_and_sets/count_symbols.py
@@ -1,8 +1,14 @@
+# input_text = input()
+# count_dict = {}
+# for letter in input_text:
+#     counter = input_text.count(letter)
+#     count_dict[letter] = counter
+# for i, v in sorted(count_dict.items(), key=lambda x: x):
+#     print(f"{i}: {v} time/s")
+
 input_text = input()
-count_dict = {}
-for letter in input_text:
-    counter = input_text.count(letter)
-    count_dict[letter] = counter
-for i, v in sorted(count_dict.items(), key=lambda x: x):
+count_dict = {char: input_text.count(char) for char in set(input_text)}
+for i, v in sorted(count_dict.items()):
     print(f"{i}: {v} time/s")
+
 
