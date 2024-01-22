700327479f827028556840308515bb98b1f2dbc1
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-01-22 22:18:37 +0200

    22.01.24

--- multidimensional_lists/diagonals.py
+++ multidimensional_lists/diagonals.py
@@ -1,18 +1,29 @@
+# matrix_size = int(input())
+# matrix = [[int(x) for x in input().split(", ")] for index in range(matrix_size)]
+#
+# first_dia = [matrix[index][index] for index in range(matrix_size)]
+# second_dia = []
+#
+# first_index = 0
+# second_index = matrix_size - 1
+#
+# for i in range(matrix_size):
+#     current_matrix = matrix[first_index][second_index]
+#     second_dia.append(current_matrix)
+#
+#     first_index += 1
+#     second_index -= 1
+#
+# print(f"Primary diagonal: {', '.join(map(str, first_dia))}. Sum: {sum(first_dia)}")
+# print(f"Secondary diagonal: {', '.join(map(str, second_dia))}. Sum: {sum(second_dia)}")
+
+# second solution:
+
 matrix_size = int(input())
 matrix = [[int(x) for x in input().split(", ")] for index in range(matrix_size)]
 
 first_dia = [matrix[index][index] for index in range(matrix_size)]
-second_dia = []
-
-first_index = 0
-second_index = matrix_size - 1
-
-for i in range(matrix_size):
-    current_matrix = matrix[first_index][second_index]
-    second_dia.append(current_matrix)
-
-    first_index += 1
-    second_index -= 1
+second_dia = [matrix[i][matrix_size - i - 1] for i in range(matrix_size)]
 
 print(f"Primary diagonal: {', '.join(map(str, first_dia))}. Sum: {sum(first_dia)}")
 print(f"Secondary diagonal: {', '.join(map(str, second_dia))}. Sum: {sum(second_dia)}")
