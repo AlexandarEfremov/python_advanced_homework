8368821904208e6bf3f56d8ae0c6ddeddd197806
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-02-01 06:00:28 +0200

    01.02.24

--- functions_advanced/age_assignment.py
+++ functions_advanced/age_assignment.py
@@ -1,8 +1,8 @@
 def age_assignment(*args, **kwargs):
     user_info = [(user, kwargs[user[0]]) for user in args]
     sorted_info = sorted(user_info, key=lambda x: x)
-    formated_result =  [f"{item[0]} is {item[1]} years old." for item in sorted_info]
-    final = "\n".join(formated_result)
+    formatted_result =  [f"{item[0]} is {item[1]} years old." for item in sorted_info]
+    final = "\n".join(formatted_result)
     return final
 
 
