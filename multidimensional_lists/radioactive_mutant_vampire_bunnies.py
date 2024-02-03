38faaef49a303074e07d245f9edc4df67c2f7170
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-02-03 17:54:35 +0200

    03.02.24

--- multidimensional_lists/radioactive_mutant_vampire_bunnies.py
+++ multidimensional_lists/radioactive_mutant_vampire_bunnies.py
@@ -1,14 +1,21 @@
-rows, cols = (int(x) for x in input().split())
+rows, cols = [int(x) for x in input().split()]
 initial_lair = []
 current_position = []
+bunny_positions = []
 
 for row in range(rows):
     line = [y for y in input()]
     initial_lair.append(line)
     if "P" in line:
         current_position = [row, initial_lair[row].index("P")]
+        initial_lair[current_position[0]][current_position[1]] = "."
+    if "B" in line:
+        for index, letter in enumerate(line):
+            if letter == "B":
+                bunny_positions.append([row, index])
 
 commands = input()
+mutated_lair = initial_lair.copy()
 
 directions = {
     "L": (0, -1),
@@ -18,40 +25,39 @@ directions = {
 }
 
 
-def bunny_multi(lair, **dir):
-    temp_lair = [row.copy() for row in lair]
-    for i, current_ro in enumerate(lair):
-        if "B" in current_ro:
-            current_bunny_position = [i, current_ro.index("B")]
-            for position in dir.keys():
-                expanded_row, expanded_col = current_bunny_position[0] + dir[position][0], \
-                    current_bunny_position[1] + dir[position][1]
-                if 0 <= expanded_row < rows and 0 <= expanded_col < rows and temp_lair[expanded_row][expanded_col]:
-                    if temp_lair[expanded_row][expanded_col] == "P":
-                        break
-                    else:
-                        temp_lair[expanded_row][expanded_col] = "B"
-    lair = temp_lair
+def bunny_mutation(lair, **directions_dict):
+    new_positions = []
+    for bunny in bunny_positions:
+        for dir in directions_dict.values():
+            new_row, new_col = bunny[0] + dir[0], bunny[1] + dir[1]
+            if 0 <= new_row < rows and 0 <= new_col < cols and lair[new_row][new_col] != "B":
+                lair[new_row][new_col] = "B"
+                new_positions.append([new_row, new_col])
+    bunny_positions.extend(new_positions)
     return lair
 
 
+current_row, current_col = current_position[0], current_position[1]
 for command in commands:
-    r, c = current_position[0] + directions[command][0], current_position[1] + directions[command][1]
-    initial_lair[current_position[0]][current_position[1]] = "."
-    if not (0 <= r < rows and 0 <= c < rows):
-        initial_lair = bunny_multi(initial_lair, **directions)
-        initial_lair = ["".join(x) for x in initial_lair]
-        [print(row, sep="\n") for row in initial_lair]
-        print(f"won: {current_position[0]} {current_position[1]}")
-        break
-    if initial_lair[r][c] == "B":
-        initial_lair = bunny_multi(initial_lair, **directions)
-        initial_lair = ["".join(x) for x in initial_lair]
-        [print(row, sep="\n") for row in initial_lair]
-        print(f"dead: {r} {c}")
-        break
+    moved_row, moved_col = current_row + directions[command][0], current_col + directions[command][1]
+    if 0 <= moved_row < rows and 0 <= moved_col < cols:
+        if mutated_lair[moved_row][moved_col] == "B":
+            mutated_lair = bunny_mutation(initial_lair, **directions)
+            [print("".join(element), sep="\n") for element in mutated_lair]
+            print(f"dead: {moved_row} {moved_col}")
+            exit()
+        else:
+            mutated_lair = bunny_mutation(initial_lair, **directions)
+            if mutated_lair[moved_row][moved_col] == "B":
+                mutated_lair = bunny_mutation(initial_lair, **directions)
+                [print("".join(element), sep="\n") for element in mutated_lair]
+                print(f"dead: {moved_row} {moved_col}")
+                exit()
     else:
-        initial_lair = bunny_multi(initial_lair, **directions)
-    current_position = [r, c]
+        mutated_lair = bunny_mutation(initial_lair, **directions)
+        [print("".join(element), sep="\n") for element in mutated_lair]
+        print(f"won: {current_row} {current_col}")
+        exit()
 
-
+    mutated_lair[current_row][current_col] = "."
+    current_row, current_col = moved_row, moved_col
