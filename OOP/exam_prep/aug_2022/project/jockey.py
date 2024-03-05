6f84ec36be167151c6d8264a022be8275ff4b766
aefremov2 <efremov.alex@yahoo.bg>
2024-03-05 16:27:57 +0200

    05.03.2024

---
+++ OOP/exam_prep/aug_2022/project/jockey.py
@@ -0,0 +1,26 @@
+class Jockey:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.horse = None
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        name_len = len([l for l in value if l.isalpha()])
+        if name_len <= 0:
+            raise ValueError("Name should contain at least one character!")
+        self.__name = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 18:
+            raise ValueError("Jockeys must be at least 18 to participate in the race!")
+        self.__age = value
