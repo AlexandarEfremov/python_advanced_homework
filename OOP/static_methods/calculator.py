e020fd2fa6235b969f925a0c7ad59b57571a9c04
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-09 09:19:23 +0200

    09.03.24

--- OOP/static_methods/calculator.py
+++ OOP/static_methods/calculator.py
@@ -1,32 +1,63 @@
+# class Calculator:
+#     @staticmethod
+#     def add(*args):
+#         return sum([arg for arg in args])
+#
+#     @staticmethod
+#     def multiply(*args):
+#         result = 1
+#         for arg in args:
+#             result *= arg
+#         return result
+#
+#     @staticmethod
+#     def divide(*args):
+#         num_list = [e for e in args]
+#         first_el = num_list.pop(0)
+#         for _ in num_list:
+#             while num_list:
+#                 first_el /= num_list.pop(0)
+#         return first_el
+#
+#     @staticmethod
+#     def subtract(*args):
+#         num_list = [e for e in args]
+#         first_el = num_list.pop(0)
+#         for _ in num_list:
+#             while num_list:
+#                 first_el -= num_list.pop(0)
+#         return first_el
+#
+# print(Calculator.add(5, 10, 4))
+#
+# print(Calculator.multiply(1, 2, 3, 5))
+#
+# print(Calculator.divide(100, 2))
+#
+# print(Calculator.subtract(90, 20, -50, 43, 7))
+from functools import reduce
+
+
+# second solution
+
+
 class Calculator:
-    @staticmethod
-    def add(*args):
-        return sum([arg for arg in args])
 
     @staticmethod
-    def multiply(*args):
-        result = 1
-        for arg in args:
-            result *= arg
-        return result
+    def add(*nums):
+        return sum(nums)
 
     @staticmethod
-    def divide(*args):
-        num_list = [e for e in args]
-        first_el = num_list.pop(0)
-        for _ in num_list:
-            while num_list:
-                first_el /= num_list.pop(0)
-        return first_el
+    def multiply(*nums):
+        return reduce(lambda x, y: x * y, nums)
 
     @staticmethod
-    def subtract(*args):
-        num_list = [e for e in args]
-        first_el = num_list.pop(0)
-        for _ in num_list:
-            while num_list:
-                first_el -= num_list.pop(0)
-        return first_el
+    def subtract(*nums):
+        return reduce(lambda x , y: x - y, nums)
+
+    @staticmethod
+    def divide(*nums):
+        return reduce(lambda x, y: x / y, nums)
 
 print(Calculator.add(5, 10, 4))
 
