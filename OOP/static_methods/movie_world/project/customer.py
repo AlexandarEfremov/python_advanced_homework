459ee42d870c7fd917179f465f718454811de322
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-12 06:52:47 +0200

    12.03.24

--- OOP/static_methods/movie_world/project/customer.py
+++ OOP/static_methods/movie_world/project/customer.py
@@ -1,10 +1,10 @@
 class Customer:
-    def __init__(self, name: str, age: int, personal_id: int):
+    def __init__(self, name: str, age: int, _id: int):
         self.name = name
         self.age = age
-        self.personal_id = personal_id
+        self.id = _id
         self.rented_dvds = []
 
     def __repr__(self):
-        return (f"{self.personal_id}: {self.name} of age {self.age} has {len(self.rented_dvds)} "
+        return (f"{self.id}: {self.name} of age {self.age} has {len(self.rented_dvds)} "
                 f"rented DVD's ({', '.join(d.name for d in self.rented_dvds)})")
--- OOP/static_methods/movie_world/project/movie_world.py
+++ OOP/static_methods/movie_world/project/movie_world.py
@@ -12,11 +12,11 @@ class MovieWorld:
         self.dvds: List[DVD] = []
 
     @staticmethod
-    def dvd_capacity(self):
+    def dvd_capacity():
         return 15
 
     @staticmethod
-    def customer_capacity(self):
+    def customer_capacity():
         return 10
 
     def add_customer(self, customer: Customer):
@@ -28,8 +28,8 @@ class MovieWorld:
             self.dvds.append(dvd)
 
     def rent_dvd(self, customer_id: int, dvd_id: int):
-        customer = next(filter(lambda c: c.personal_id == customer_id, self.customers))
-        dvd = next(filter(lambda d: d.dvd_id == dvd_id, self.dvds))
+        customer = next(filter(lambda c: c.id == customer_id, self.customers))
+        dvd = next(filter(lambda d: d.id == dvd_id, self.dvds))
 
         if dvd in customer.rented_dvds:
             return f"{customer.name} has already rented {dvd.name}"
@@ -38,17 +38,19 @@ class MovieWorld:
         if customer.age < dvd.age_restriction:
             return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
         else:
-            customer.rented_dvds.append(dvd.dvd_id)
+            customer.rented_dvds.append(dvd)
+            dvd.is_rented = True
             return f"{customer.name} has successfully rented {dvd.name}"
 
     def return_dvd(self, customer_id, dvd_id):
-        customer = next(filter(lambda c: c.personal_id == customer_id, self.customers))
-        dvd = next(filter(lambda d: d.dvd_id == dvd_id, self.dvds))
+        customer = next(filter(lambda c: c.id == customer_id, self.customers))
+        dvd = next(filter(lambda d: d.id == dvd_id, self.dvds))
 
-        if dvd.dvd_id not in customer.rented_dvds:
+        if dvd not in customer.rented_dvds:
             return f"{customer.name} does not have that DVD"
         else:
-            customer.rented_dvds.remove(dvd.dvd_id)
+            customer.rented_dvds.remove(dvd)
+            dvd.is_rented = False
             return f"{customer.name} has successfully returned {dvd.name}"
 
     def __repr__(self):
