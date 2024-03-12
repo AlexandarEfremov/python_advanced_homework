3c9a88d18ecdae01e0aa36c75b173ae79c8e7503
aefremov2 <efremov.alex@yahoo.bg>
2024-03-12 12:07:33 +0200

    12.03.2024

--- OOP/static_methods/document_management/project/storage.py
+++ OOP/static_methods/document_management/project/storage.py
@@ -10,23 +10,43 @@ class Storage:
         self.documents = []
 
     def add_category(self, category: Category):
-        if category.id not in self.categories:
-            self.categories.append(category.id)
+        if category not in self.categories:
+            self.categories.append(category)
 
     def add_topic(self, topic: Topic):
-        if topic.id not in self.topics:
-            self.categories.append(topic.id)
+        if topic not in self.topics:
+            self.topics.append(topic)
 
     def add_document(self, document: Document):
-        if document.id not in self.documents:
-            self.categories.append(document.id)
+        if document not in self.documents:
+            self.documents.append(document)
 
     def edit_category(self, category_id: int, new_name: str):
         category = next(filter(lambda c: c.id == category_id, self.categories))
         category.edit(new_name)
 
+    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
+        topic = next(filter(lambda t: t.id == topic_id, self.topics))
+        topic.edit(new_topic, new_storage_folder)
+
     def edit_document(self, document_id: int, new_file_name: str):
         document = next((filter(lambda d: d.id == document_id, self.documents)))
         document.edit(new_file_name)
 
+    def delete_category(self, category_id):
+        category = next(filter(lambda c: c.id == category_id, self.categories))
+        self.categories.remove(category)
 
+    def delete_topic(self, topic_id):
+        topic = next((filter(lambda t: t.id == topic_id, self.topics)))
+        self.topics.remove(topic)
+
+    def delete_document(self, document_id):
+        document = next((filter(lambda d: d.id == document_id, self.documents)))
+        self.documents.remove(document)
+
+    def get_document(self, document_id):
+        return next((filter(lambda d: d.id == document_id, self.documents)))
+
+    def __repr__(self):
+        return "\n".join(str(d) for d in self.documents)
