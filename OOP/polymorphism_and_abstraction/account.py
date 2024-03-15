d4ccbd18f7de02b481adc78ba080e3aeb92ce054
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-15 05:42:40 +0200

    15.03.24

--- OOP/polymorphism_and_abstraction/account.py
+++ OOP/polymorphism_and_abstraction/account.py
@@ -4,22 +4,23 @@ class Account:
         self.amount = amount
         self._transactions = []
 
-    def handle_transaction(self, transaction_amount):
-        if self.amount >= transaction_amount:
-            self._transactions.append(transaction_amount)
-            return f"New balance: {self.amount}"
-        else:
+    @property
+    def balance(self):
+        return sum(self._transactions) + self.amount
+
+    def handle_transaction(self, transaction_amount: int) -> str:
+        if self.balance + transaction_amount < 0:
             raise ValueError("sorry cannot go in debt!")
+
+        self._transactions.append(transaction_amount)
+
+        return f"New balance: {self.balance}"
 
     def add_transaction(self, amount):
         if not isinstance(amount, int):
             raise ValueError("please use int for amount")
         else:
             return self.handle_transaction(amount)
-
-    @property
-    def balance(self):
-        return sum(self._transactions) + self.amount
 
     def __str__(self):
         return f"Account of {self.owner} with starting amount: {self.amount}"
@@ -31,10 +32,10 @@ class Account:
         return len(self._transactions)
 
     def __getitem__(self, item):
-        return item
+        return self._transactions[item]
 
     def __reversed__(self):
-        return self._transactions.reverse()
+        return reversed(self._transactions)
 
     def __gt__(self, other):
         return self.balance > other.balance
@@ -55,8 +56,10 @@ class Account:
         return self.balance != other.balance
 
     def __add__(self, other):
-        return Account(f"{self.owner}&{other.owner} with starting amount: "
-                       f"{self.amount + other.amount}", self.owner + other.owner)
+        new_account = Account(f"{self.owner}&{other.owner}", self.amount + other.amount)
+        new_account._transactions = self._transactions + other._transactions
+
+        return new_account
 
 
 acc = Account('bob', 10)
