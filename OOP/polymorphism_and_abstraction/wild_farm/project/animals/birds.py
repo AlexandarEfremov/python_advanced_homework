27c5df6f2d580a2c6308a4dc248d7a7202d513c6
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-15 07:28:06 +0200

    15.03.24

---
+++ OOP/polymorphism_and_abstraction/wild_farm/project/animals/birds.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+from project.animals.animal import Bird
+from project.food import Meat, Seed, Vegetable, Fruit
+
+
+class Owl(Bird):
+    def make_sound(self):
+        return "Hoot Hoot"
+
+    @property
+    def gained_weight(self):
+        return 0.25
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+
+class Hen(Bird):
+    def make_sound(self):
+        return "Cluck"
+
+    @property
+    def food_that_eats(self):
+        return [Meat, Vegetable, Fruit, Seed]
+
+    @property
+    def gained_weight(self):
+        return 0.35
+
---
+++ OOP/polymorphism_and_abstraction/wild_farm/project/animals/mammals.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+from project.animals.animal import Mammal
+from project.food import Meat, Seed, Vegetable, Fruit
+
+
+class Mouse(Mammal):
+    def make_sound(self):
+        return "Squeak"
+
+    @property
+    def food_that_eats(self):
+        return [Vegetable, Fruit]
+
+    @property
+    def gained_weight(self):
+        return 0.10
+
+
+class Dog(Mammal):
+    def make_sound(self):
+        return "Woof!"
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.40
+
+
+class Tiger(Mammal):
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    def make_sound(self):
+        return "ROAR!!!"
+
+    @property
+    def gained_weight(self):
+        return 1.00
+
+
+class Cat(Mammal):
+    def make_sound(self):
+        return "Meow"
+
+    @property
+    def food_that_eats(self):
+        return [Meat, Vegetable]
+
+    @property
+    def gained_weight(self):
+        return 0.30
