319faa1b5ad6b13363775e1f3d3ab94365061b39
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-20 05:58:23 +0200

    20.03.24

---
+++ OOP/iterators_and_generators/countdown_iterator.py
@@ -0,0 +1,20 @@
+class countdown_iterator:
+    def __init__(self, count: int):
+        self.count = count
+        self.current = self.count + 1
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current == 0:
+            raise StopIteration
+
+        self.current -= 1
+
+        return self.current
+
+
+iterator = countdown_iterator(0)
+
+for item in iterator:
+    print(item, end=" ")
