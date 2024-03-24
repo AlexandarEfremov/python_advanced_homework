0dbbaf7cb6401dbf697513839a7ec73af99d8a37
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-24 21:55:48 +0200

    24.03.24

--- OOP/decorators/computer_store/project/computer_types/desktop_computer.py
+++ OOP/decorators/computer_store/project/computer_types/desktop_computer.py
@@ -11,19 +11,7 @@ class DesktopComputer(Computer):
 
     def available_processors(self):
         return {
-        "AMD Ryzen 7 5700G": 500,
-        "Intel Core i5-12600K": 600,
-        "Apple M1 Max": 1800,
-    }
-
-    def configure_computer(self, processor: str, ram: int):
-        if processor not in self.AVAILABLE_PROCESSORS:
-            raise ValueError(f"{processor} is not compatible with desktop computer {self.manufacturer} {self.model}!")
-        if ram not in self.VALID_RAM:
-            raise ValueError(f"{ram}GB RAM is not compatible with desktop computer {self.manufacturer} {self.model}!")
-        self.processor = processor
-        self.ram = ram
-        price = self.AVAILABLE_PROCESSORS[processor] + (math.log(2, self.ram)) * 100
-        self.price = price
-        return (f"Created {self.manufacturer} {self.model} with {self.processor} and "
-                f"{self.ram}GB RAM for {price}$.")
+            "AMD Ryzen 7 5700G": 500,
+            "Intel Core i5-12600K": 600,
+            "Apple M1 Max": 1800,
+        }
