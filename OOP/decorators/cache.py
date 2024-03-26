f7ad9e4fdfc94877313b5213fbe2f7e91c986387
aefremov2 <efremov.alex@yahoo.bg>
2024-03-26 10:10:49 +0200

    22.03.2024

--- OOP/decorators/cache.py
+++ OOP/decorators/cache.py
@@ -1,19 +1,14 @@
 def cache(func):
-    def wrapper(num):
-        if not wrapper.log.get(num):
-            wrapper.log[num] = func(num)
+    pass
 
-        return wrapper.log[num]
-
-    wrapper.log = {}
-
-    return wrapper
 @cache
 def fibonacci(n):
     if n < 2:
         return n
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
 
 fibonacci(3)
 
