f5fb3f886ed80532c4f87cad5c19fe3ded5e5ab0
aefremov2 <efremov.alex@yahoo.bg>
2024-03-27 18:01:35 +0200

    27.03.2024

--- OOP/exam_prep/dec_2023/project/peaks/arctic_peak.py
+++ OOP/exam_prep/dec_2023/project/peaks/arctic_peak.py
@@ -2,3 +2,16 @@ from project.peaks.base_peak import BasePeak
 
 
 class ArcticPeak(BasePeak):
+    RECOMMENDED_GEAR = ["Ice axe", "Crampons", "Insulated clothing", "Helmet"]
+
+    def __init__(self, name: str, elevation: int):
+        super().__init__(name, elevation)
+
+    def get_recommended_gear(self):
+        return self.RECOMMENDED_GEAR
+
+    def calculate_difficulty_level(self):
+        return "Extreme" if self.elevation > 3000 else "Advanced"
+
+
+
