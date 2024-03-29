3ed347a7ae46eff188060751a836cc2a9a14fe5f
aefremov2 <efremov.alex@yahoo.bg>
2024-03-29 11:19:14 +0200

    29.03.2024

--- OOP/exam_prep/19_dec_2023/project/summit_quest_manager_app.py
+++ OOP/exam_prep/19_dec_2023/project/summit_quest_manager_app.py
@@ -1,15 +1,73 @@
+from typing import List
+
+from project.climbers.arctic_climber import ArcticClimber
+from project.climbers.summit_climber import SummitClimber
+from project.peaks.arctic_peak import ArcticPeak
+from project.peaks.summit_peak import SummitPeak
+
+
 class SummitQuestManagerApp:
-    CLIMBER_TYPES = ["ArcticClimber", "SummitClimber"]
+    CLIMBER_TYPES = {
+        "ArcticClimber": ArcticClimber,
+        "SummitClimber": SummitClimber
+    }
+
+    PEAK_TYPES = {
+        "ArcticPeak": ArcticPeak,
+        "SummitPeak": SummitPeak
+    }
 
     def __init__(self, climbers: list, peaks: list):
         self.climbers = climbers
         self.peaks = peaks
 
     def register_climber(self, climber_type: str, climber_name: str):
-        if climber_type not in self.CLIMBER_TYPES:
+        try:
+            climber = self.CLIMBER_TYPES[climber_type](climber_name)
+        except KeyError:
             return f"{climber_type} doesn't exist in our register."
-        if climber_name in self.climbers:
+
+        try:
+            next(filter(lambda c: c.name == climber_name, self.climbers))
             return f"{climber_name} has been already registered."
-        self.climbers.append(climber_name)
-        return f"{climber_name} is successfully registered as a {climber_type}."
+        except StopIteration:
+            self.climbers.append(climber)
+            return f"{climber_name} is successfully registered as a {climber_type}."
+
+
+    def peak_wish_list(self, peak_type: str, peak_name: str, peak_elevation: int):
+        try:
+            peak = self.PEAK_TYPES[peak_type](peak_name, peak_elevation)
+        except KeyError:
+            return f"{peak_type} is an unknown type of peak."
+
+        self.peaks.append(peak)
+        return f"{peak_name} is successfully added to the wish list as a {peak_type}."
+
+    def check_gear(self, climber_name: str, peak_name: str, gear: List[str]):
+        climber = next(filter(lambda c: c.name == climber_name, self.climbers))
+        peak = next(filter(lambda p: p.name == peak_name, self.peaks))
+
+        if gear == peak.get_recommended_gear():
+            return f"{climber_name} is prepared to climb {peak_name}."
+        climber.is_prepared = False
+        return (f"{climber_name} is not prepared to climb {peak_name}. Missing gear: "
+                f"{', '.join(g for g in sorted(peak.get_recommended_gear()) if g not in gear)}.")
+
+    def perform_climbing(self, climber_name: str, peak_name: str):
+        try:
+            climber = next(filter(lambda c: c.name == climber_name, self.climbers))
+        except StopIteration:
+            return f"Climber {climber_name} is not registered yet."
+
+        try:
+            peak = next(filter(lambda p: p.name == peak_name, self.peaks))
+        except StopIteration:
+            return f"Peak {peak_name} is not part of the wish list."
+
+        if not climber.can_climb():
+            return "{climber_name} needs more strength to climb {peak_name} and is therefore taking some rest."
+
+        return f"{climber_name} conquered {peak_name} whose difficulty level is {peak.calculate_difficulty_level()}."
+
 
