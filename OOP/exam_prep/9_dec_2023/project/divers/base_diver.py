6f8b8aa017b3b48e26cf843f5ba18c555542a26a
aefremov2 <efremov.alex@yahoo.bg>
2024-03-29 18:50:31 +0200

    29.03.2024

--- OOP/exam_prep/9_dec_2023/project/divers/base_diver.py
+++ OOP/exam_prep/9_dec_2023/project/divers/base_diver.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from project.fish.base_fish import BaseFish
+
 
 class BaseDiver(ABC):
     def __init__(self, name: str, oxygen_level: float):
@@ -28,4 +30,31 @@ class BaseDiver(ABC):
         if value < 0:
             raise ValueError("Cannot create diver with negative oxygen level!")
         self.__oxygen_level = value
+
+    @abstractmethod
+    def miss(self, time_to_catch: int):
+        ...
+
+    @abstractmethod
+    def renew_oxy(self):
+        ...
+
+    def hit(self, fish: BaseFish):
+        if self.oxygen_level - fish.time_to_catch > 0:
+            self.oxygen_level -= fish.time_to_catch
+            self.catch.append(fish)
+            self.competition_points += round(fish.points, 1)
+        else:
+            self.oxygen_level = 0
+
+    def update_health_status(self):
+        if self.has_health_issue:
+            self.has_health_issue = False
+            return self.has_health_issue
+        else:
+            self.has_health_issue = True
+            return self.has_health_issue
+
+
+
 
