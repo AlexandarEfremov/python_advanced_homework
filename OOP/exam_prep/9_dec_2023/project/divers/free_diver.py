080341b03efaaedeab789c3adb9e5950a1cffd78
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-03-30 15:17:31 +0200

    30.03.24

---
+++ OOP/exam_prep/9_dec_2023/project/divers/free_diver.py
@@ -0,0 +1,25 @@
+from project.divers.base_diver import BaseDiver
+from math import ceil
+
+
+class FreeDiver(BaseDiver):
+
+    def __init__(self, name: str):
+        super().__init__(name, oxygen_level=120)
+
+    def miss(self, time_to_catch: int):
+        number = 0.6 * time_to_catch
+        if isinstance(number, float):
+            if self.oxygen_level >= ceil(number):
+                self.oxygen_level -= ceil(number)
+            else:
+                self.oxygen_level = 0
+        else:
+            if self.oxygen_level >= number:
+                self.oxygen_level -= number
+            else:
+                self.oxygen_level = 0
+
+
+    def renew_oxy(self):
+        pass
