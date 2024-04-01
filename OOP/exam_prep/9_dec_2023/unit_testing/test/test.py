799f57523b17577f3df4905bc55d86948119e312
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-01 09:33:09 +0300

    31.03.24

--- OOP/exam_prep/9_dec_2023/unit_testing/test/test.py
+++ OOP/exam_prep/9_dec_2023/unit_testing/test/test.py
@@ -19,8 +19,19 @@ class TestRailwayStation(TestCase):
             self.train = RailwayStation("Aex")
         self.assertEqual("Name should be more than 3 symbols!", str(ex.exception))
 
+    def test_append_new_train(self):
+        self.train.arrival_trains.append("d")
+        self.assertEqual(["a", "b", "c", "d"], self.train.arrival_trains)
 
+    def test_other_trains_before_info(self):
+        train_info = "c"
+        self.assertEqual(f"There are other trains to arrive before {train_info}.",
+                         self.train.train_has_arrived(train_info))
 
+    def test_info_train_is_next(self):
+        train_info = "a"
+        self.assertEqual(f"{train_info} is on the platform and will leave in 5 minutes.",
+                         self.train.train_has_arrived(train_info))
 
 if __name__ == "__main__":
     main()
