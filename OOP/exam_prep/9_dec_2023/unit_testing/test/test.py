2778e757862e6f528ba7994f5099afd1bb5b22d8
aefremov2 <efremov.alex@yahoo.bg>
2024-04-01 09:37:01 +0300

    29.03.2024

--- OOP/exam_prep/9_dec_2023/unit_testing/test/test.py
+++ OOP/exam_prep/9_dec_2023/unit_testing/test/test.py
@@ -1,13 +1,13 @@
 from unittest import TestCase, main
-
+from collections import deque
 from project.railway_station import RailwayStation
 
 
 class TestRailwayStation(TestCase):
     def setUp(self):
         self.train = RailwayStation("Alex")
-        self.train.arrival_trains = ["a", "b", "c"]
-        self.train.departure_trains = ["x", "y", "z"]
+        self.train.arrival_trains = deque(["a", "b", "c"])
+        self.train.departure_trains = deque(["x", "y", "z"])
 
     def test_correct__init__(self):
         self.assertEqual("Alex", self.train.name)
@@ -18,6 +18,11 @@ class TestRailwayStation(TestCase):
         with self.assertRaises(Exception) as ex:
             self.train = RailwayStation("Aex")
         self.assertEqual("Name should be more than 3 symbols!", str(ex.exception))
+
+    def test_new_arrival_on_board(self):
+        train_info = "Ewcia"
+        self.train.new_arrival_on_board(train_info)
+        self.assertEqual(['a', 'b', 'c', 'Ewcia'], self.train.arrival_trains)
 
     def test_append_new_train(self):
         self.train.arrival_trains.append("d")
