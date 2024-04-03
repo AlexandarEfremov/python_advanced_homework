1fb48d869c2fa220c9ed7afb5c9507377c5aeb3e
aefremov2 <efremov.alex@yahoo.bg>
2024-04-03 14:53:04 +0300

    03.04.2024

--- OOP/exam_prep/5_aug_2023/project/bank_app.py
+++ OOP/exam_prep/5_aug_2023/project/bank_app.py
@@ -1,6 +1,8 @@
 from typing import List
 
+from project.clients.adult import Adult
 from project.clients.base_client import BaseClient
+from project.clients.student import Student
 from project.loans.base_loan import BaseLoan
 from project.loans.mortgage_loan import MortgageLoan
 from project.loans.student_loan import StudentLoan
@@ -10,6 +12,11 @@ class BankApp:
     LOAN_TYPES = {
         "StudentLoan": StudentLoan,
         "MortgageLoan": MortgageLoan,
+    }
+
+    CLIENT_TYPE = {
+        "Student": Student,
+        "Adult": Adult,
     }
 
     def __init__(self, capacity: int):
@@ -23,5 +30,14 @@ class BankApp:
         loan = self.LOAN_TYPES[loan_type]()
         self.loans.append(loan)
         return f"{loan_type} was successfully added."
+
+    def add_client(self, client_type: str, client_name: str, client_id: str, income: float):
+        if client_type not in self.CLIENT_TYPE:
+            raise Exception("Invalid client type!")
+        if self.capacity == len(self.clients):
+            return "Not enough bank capacity."
+        client = self.CLIENT_TYPE[client_type](client_name, client_id, income)
+        self.clients.append(client)
+        return f"{client_type} was successfully added."
 
 
