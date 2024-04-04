27828b58bfb89f15853c865e92b77860d8399322
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-04 08:10:34 +0300

    04.04.24

--- OOP/exam_prep/5_aug_2023/unit_testing/project/test/test.py
+++ OOP/exam_prep/5_aug_2023/unit_testing/project/test/test.py
@@ -8,12 +8,27 @@ class TestSecondHandCar(TestCase):
         self.car = SecondHandCar("Ford", "hatchback", 1_000_000, 10_000.00)
         self.other = SecondHandCar("Volvo", "combi", 500_000, 20_000.00)
         self.other_hatchback = SecondHandCar("Seat", "hatchback", 400_000, 8_000.00)
+
     def test_correct__init__(self):
         self.assertEqual("Ford", self.car.model)
         self.assertEqual("hatchback", self.car.car_type)
         self.assertEqual(1_000_000, self.car.mileage)
         self.assertEqual(10_000.00, self.car.price)
         self.assertEqual([], self.car.repairs)
+
+    def test_correct__init__other(self):
+        self.assertEqual("Volvo", self.other.model)
+        self.assertEqual("combi", self.other.car_type)
+        self.assertEqual(500_000, self.other.mileage)
+        self.assertEqual(20_000.00, self.other.price)
+        self.assertEqual([], self.other.repairs)
+
+    def test_correct__init__other_hatchback(self):
+        self.assertEqual("Seat", self.other_hatchback.model)
+        self.assertEqual("hatchback", self.other_hatchback.car_type)
+        self.assertEqual(400_000, self.other_hatchback.mileage)
+        self.assertEqual(8000.00, self.other_hatchback.price)
+        self.assertEqual([], self.other_hatchback.repairs)
 
     def test_minimum_price_expect_error(self):
         with self.assertRaises(Exception) as ex:
@@ -38,12 +53,24 @@ class TestSecondHandCar(TestCase):
         self.assertEqual('The promotional price has been successfully set.',
                          self.car.set_promotional_price(5_000.00))
 
+    def test_changed_promotional_price(self):
+        self.car.set_promotional_price(5_000.00)
+        self.assertEqual(5_000.00, self.car.price)
+
     def test_repair_if_repair_price_is_more_than_half_expect_error(self):
         self.assertEqual('Repair is impossible!', self.car.need_repair(5_001.00, "wheels"))
 
     def test_repair_if_price_is_less_than_half_expect_success(self):
         self.assertEqual('Price has been increased due to repair charges.',
                          self.car.need_repair(4_001.00, "wheels"))
+
+    def test_price_after_repairs_expect_increase(self):
+        self.car.need_repair(5_000.00, "wheels")
+        self.assertEqual(15_000.00, self.car.price)
+
+    def test_repair_parts_in_list_after_repair(self):
+        self.car.need_repair(5_000.00, "wheels")
+        self.assertEqual(["wheels"], self.car.repairs)
 
     def test__gt__expect_negative(self):
         self.assertEqual('Cars cannot be compared. Type mismatch!', self.car > self.other)
