d6448a02c5204f38835b4e308244e9209f10b95d
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-04 07:41:33 +0300

    04.04.24

--- OOP/exam_prep/5_aug_2023/unit_testing/project/test/test.py
+++ OOP/exam_prep/5_aug_2023/unit_testing/project/test/test.py
@@ -27,6 +27,23 @@ class TestSecondHandCar(TestCase):
         self.assertEqual('Please, second-hand cars only! Mileage must be greater than 100!',
                          str(ex.exception))
 
+    def test_setting_promotional_offer_expect_error(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.set_promotional_price(20_000.00)
+
+        self.assertEqual('You are supposed to decrease the price!', str(ex.exception))
+
+    def test_setting_promotional_offer_expect_success(self):
+        self.assertEqual('The promotional price has been successfully set.',
+                         self.car.set_promotional_price(5_000.00))
+
+    def test_repair_if_repair_price_is_more_than_half_expect_error(self):
+        self.assertEqual('Repair is impossible!', self.car.need_repair(5_001.00, "wheels"))
+
+    def test_repair_if_price_is_less_than_half_expect_success(self):
+        self.assertEqual('Price has been increased due to repair charges.',
+                         self.car.need_repair(4_001.00, "wheels"))
+
 
 if __name__ == "__main__":
     main()
