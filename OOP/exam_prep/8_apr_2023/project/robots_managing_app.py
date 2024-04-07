b8cda20283e6ee099c9eb45b5802fb51e09ce9d2
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-07 21:51:17 +0300

    07.04.24

--- OOP/exam_prep/8_apr_2023/project/robots_managing_app.py
+++ OOP/exam_prep/8_apr_2023/project/robots_managing_app.py
@@ -38,8 +38,8 @@ class RobotsManagingApp:
         return f"{robot_type} is successfully added."
 
     def add_robot_to_service(self, robot_name: str, service_name: str):
-        robot = next(r for r in self.robots if r.name == robot_name)
-        service = next(s for s in self.services if s.name == service_name)
+        robot = next((r for r in self.robots if r.name == robot_name), None)
+        service = next((s for s in self.services if s.name == service_name), None)
 
         if ((robot.__class__.__name__ == "FemaleRobot" and service.__class__.__name__ != "SecondaryService")
                 or (robot.__class__.__name__ == "MaleRobot" and service.__class__.__name__ != "MainService")):
@@ -51,3 +51,24 @@ class RobotsManagingApp:
         self.robots.remove(robot)
         service.robots.append(robot)
         return f"Successfully added {robot.name} to {service.name}."
+
+    def remove_robot_from_service(self, robot_name: str, service_name: str):
+        robot = next((r for r in self.robots if r.name == robot_name), None)
+        service = next((s for s in self.services if s.name == service_name), None)
+        if robot is None:
+            raise Exception("No such robot in this service!")
+
+        service.robots.remove(robot)
+        self.robots.append(robot)
+        return f"Successfully removed {robot.name} from {service.name}."
+
+    def feed_all_robots_from_service(self, service_name: str):
+        service = next((s for s in self.services if s.name == service_name), None)
+        robots_fed = 0
+        for robot in service.robots:
+            robot.eating()
+            robots_fed += 1
+        return f"Robots fed: {robots_fed}."
+
+
+
