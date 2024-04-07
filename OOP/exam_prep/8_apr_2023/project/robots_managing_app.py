68089673448b71cb20a9b7d080a8b42cf8909465
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-07 21:36:14 +0300

    07.04.24

--- OOP/exam_prep/8_apr_2023/project/robots_managing_app.py
+++ OOP/exam_prep/8_apr_2023/project/robots_managing_app.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from project.robots.base_robot import BaseRobot
+from project.robots.female_robot import FemaleRobot
+from project.robots.male_robot import MaleRobot
 from project.services.base_service import BaseService
 from project.services.main_service import MainService
 from project.services.secondary_service import SecondaryService
@@ -10,6 +12,11 @@ class RobotsManagingApp:
     VALID_SERVICE_TYPES = {
         "MainService": MainService,
         "SecondaryService": SecondaryService,
+    }
+
+    VALID_ROBOT_TYPES = {
+        "MaleRobot": MaleRobot,
+        "FemaleRobot": FemaleRobot
     }
 
     def __init__(self):
@@ -23,3 +30,9 @@ class RobotsManagingApp:
         self.services.append(service)
         return f"{service_type} is successfully added."
 
+    def add_robot(self, robot_type: str, name: str, kind: str, price: float):
+        if robot_type not in self.VALID_ROBOT_TYPES:
+            raise Exception("Invalid robot type!")
+        robot = self.VALID_ROBOT_TYPES[robot_type](name, kind, price)
+        self.robots.append(robot)
+        return f"{robot_type} is successfully added."
