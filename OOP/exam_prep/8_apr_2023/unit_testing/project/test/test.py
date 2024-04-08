dc57973309d6f508bee457809b8f55d2fec6de1d
aefremov2 <efremov.alex@yahoo.bg>
2024-04-08 14:45:23 +0300

    08.04.2024

--- OOP/exam_prep/8_apr_2023/unit_testing/project/test/test.py
+++ OOP/exam_prep/8_apr_2023/unit_testing/project/test/test.py
@@ -6,6 +6,13 @@ from project.tennis_player import TennisPlayer
 class TestTennisPlayer(TestCase):
     def setUp(self):
         self.player = TennisPlayer("Alex", 31, 400)
+        self.other_player = TennisPlayer("Ewcia", 33, 500)
+
+    def test_correct_other_init(self):
+        self.assertEqual("Ewcia", self.other_player.name)
+        self.assertEqual(33, self.other_player.age)
+        self.assertEqual(500, self.other_player.points)
+        self.assertEqual([], self.other_player.wins)
 
     def test_correct_init(self):
         self.assertEqual("Alex", self.player.name)
@@ -25,6 +32,15 @@ class TestTennisPlayer(TestCase):
 
         self.assertEqual("Players must be at least 18 years of age!", str(ex.exception))
 
+    def test_tournament_in_tournament_list_bad(self):
+        self.player.add_new_win("Wimbeldon")
+        result = self.player.add_new_win("Wimbeldon")
+
+        self.assertEqual("Wimbeldon has been already added to the list of wins!", result)
+
+    def test_tournament_not_in_tournament_list_good(self):
+        self.player.add_new_win("Wimbeldon")
+        self.assertEqual(["Wimbeldon"], self.player.wins)
 
 if __name__ == "__main__":
     main()
