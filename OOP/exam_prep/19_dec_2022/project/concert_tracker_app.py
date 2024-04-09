4b9638deed4ba33c984f36d10c2bd564f0112e6b
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-09 07:23:43 +0300

    09.04.2024

---
+++ OOP/exam_prep/19_dec_2022/project/concert_tracker_app.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from project.band import Band
+from project.band_members.musician import Musician
+from project.concert import Concert
+
+
+class ConcertTrackerApp:
+    def __init__(self):
+        self.bands: List[Band] = []
+        self.musicians: List[Musician] = []
+        self.concerts: List[Concert] = []
+
+    def create_musician(self, musician_type: str, name: str, age: int)
