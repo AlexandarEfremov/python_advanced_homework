26659ac210fbfaf56f16615caead1963f4eda8f4
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-12 09:21:00 +0300

    12.04.24

--- OOP/exam_prep/6_apr_2024/project/influencer_manager_app.py
+++ OOP/exam_prep/6_apr_2024/project/influencer_manager_app.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from project.campaigns.base_campaign import BaseCampaign
+from project.campaigns.high_budget_campaign import HighBudgetCampaign
+from project.campaigns.low_budget_campaign import LowBudgetCampaign
 from project.influencers.base_influencer import BaseInfluencer
 from project.influencers.premium_influencer import PremiumInfluencer
 from project.influencers.standard_influencer import StandardInfluencer
@@ -12,6 +14,10 @@ class InfluencerManagerApp:
         "StandardInfluencer": StandardInfluencer,
     }
 
+    VALID_CAMPAIGN_TYPES = {
+        "HighBudgetCampaign": HighBudgetCampaign,
+        "LowBudgetCampaign": LowBudgetCampaign,
+    }
     def __init__(self):
         self.influencers: List[BaseInfluencer] = []
         self.campaigns: List[BaseCampaign] = []
@@ -25,4 +31,14 @@ class InfluencerManagerApp:
         inf = self.VALID_INFLUENCER_TYPES[influencer_type](username, followers, engagement_rate)
         self.influencers.append(inf)
         return f"{username} is successfully registered as a {influencer_type}."
+
+    def create_campaign(self, campaign_type: str, campaign_id: int, brand: str, required_engagement: float):
+        if campaign_type not in self.VALID_CAMPAIGN_TYPES:
+            return f"{campaign_type} is not a valid campaign type."
+        camp = next((c for c in self.campaigns if c.campaign_id == campaign_id), None)
+        if camp:
+            return f"Campaign ID {campaign_id} has already been created."
+        new_campaign = self.VALID_CAMPAIGN_TYPES[campaign_type](campaign_id, brand, required_engagement)
+        self.campaigns.append(new_campaign)
+        return f"Campaign ID {campaign_id} for {brand} is successfully created as a {campaign_type}."
 
