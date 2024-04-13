33f7759bd15cd1542d045adcafee40785311e502
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-13 13:00:19 +0300

    13.04.24

--- OOP/exam_prep/22_aug_2022/project/horse_race_app.py
+++ OOP/exam_prep/22_aug_2022/project/horse_race_app.py
@@ -46,3 +46,29 @@ class HorseRaceApp:
             self.AVAILABLE_RACES.remove(race_type)
             self.horse_races.append(race)
             return f"Race {race_type} is created."
+
+    def add_horse_to_jockey(self, jockey_name: str, horse_type: str):
+        jockey = next((j for j in self.jockeys if j.name == jockey_name), None)
+        if jockey is None:
+            raise Exception(f"Jockey {jockey_name} could not be found!")
+        horse = next((h for h in self.horses if h.__class__.__name__ == horse_type and h.is_taken is False), None)
+        if horse is None:
+            raise Exception(f"Horse breed {horse_type} could not be found!")
+        if horse and jockey.horse:
+            raise Exception(f"Jockey {jockey_name} already has a horse.")
+        jockey.horse = horse
+        return f"Jockey {jockey_name} will ride the horse {horse.name}."
+
+    def add_jockey_to_horse_race(self, race_type: str, jockey_name: str):
+        wanted_race = next((r for r in self.horse_races if r.__class__.__name__ == race_type), None)
+        if wanted_race is None:
+            raise Exception(f"Race {race_type} could not be found!")
+        jockey = next((j for j in self.jockeys if j.name == jockey_name), None)
+        if jockey is None:
+            raise Exception(f"Jockey {jockey_name} could not be found!")
+        if jockey.horse is None:
+            raise Exception(f"Jockey {jockey_name} cannot race without a horse!")
+        if jockey in wanted_race.jockeys:
+            raise Exception(f"Jockey {jockey_name} has been already added to the {race_type} race.")
+        wanted_race.jockeys.append(jockey)
+        return f"Jockey {jockey_name} added to the {race_type} race."
