f46e3bcd3758b8bf0bddc1d5d32106d016b084f5
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-22 05:30:42 +0300

    22.04.24

--- OOP/exam_prep/10_apr_2022/project/controller.py
+++ OOP/exam_prep/10_apr_2022/project/controller.py
@@ -24,7 +24,26 @@ class Controller:
             self.supplies.append(supp)
 
     def sustain(self, player_name: str, sustenance_type: str):
-        pass
+        if sustenance_type == "Food" or sustenance_type == "Drink":
+            player = next((p for p in self.players if p.name == player_name), None)
+            if player:
+                if player.stamina == 100:
+                    return f"{player_name} have enough stamina."
+                if sustenance_type == "Food":
+                    food_available = next((f for f in reversed(self.supplies) if f.__class__.__name__ == "Food"), None)
+                    if food_available is None:
+                        raise Exception("There are no food supplies left!")
+                    else:
+                        player.stamina += food_available.energy
+                        self.supplies.remove(food_available)
+                        if player.stamina > 100:
+                            player.stamina = 100
+                elif sustenance_type == "Drink":
+                    drink_available = next((d for d in reversed(self.supplies) if d.__class__.__name__ == "Drink"), None)
+                    if drink_available is None:
+                        raise Exception("There are no drink supplies left!")
+
+
 
     def duel(self, first_player_name: str, second_player_name: str):
         pass
