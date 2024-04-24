ab8259a781bb03fdda73563979481798f327ba1b
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-24 06:25:18 +0300

    24.04.24

--- OOP/exam_prep/11_dec_2021/project/controller.py
+++ OOP/exam_prep/11_dec_2021/project/controller.py
@@ -1,17 +1,30 @@
 from typing import List
 from project.car.car import Car
+from project.car.muscle_car import MuscleCar
+from project.car.sports_car import SportsCar
 from project.driver import Driver
 from project.race import Race
 
 
 class Controller:
+    VALID_CAR_TYPES = {
+        "MuscleCar": MuscleCar,
+        "SportsCar": SportsCar,
+    }
+
     def __init__(self):
         self.cars: List[Car] = []
         self.drivers: List[Driver] = []
         self.races: List[Race] = []
 
     def create_car(self, car_type: str, model: str, speed_limit: int):
-        pass
+        if car_type in self.VALID_CAR_TYPES:
+            car_match = next((c for c in self.cars if c.model == model), None)
+            if car_match:
+                raise Exception(f"Car {model} is already created!")
+            car = self.VALID_CAR_TYPES[car_type](model, speed_limit)
+            self.cars.append(car)
+            return f"{car_type} {model} is created."
 
     def create_driver(self, driver_name: str):
         pass
