be5a8ad5cdf30c81fff1b2f369587cf7f4429bd0
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-04-24 06:58:10 +0300

    24.04.24

--- OOP/exam_prep/11_dec_2021/project/controller.py
+++ OOP/exam_prep/11_dec_2021/project/controller.py
@@ -42,12 +42,36 @@ class Controller:
         self.races.append(race)
         return f"Race {race_name} is created."
 
+    def __check_if_driver_exists(self, name):
+        driver_match = next((d for d in self.drivers if d.name == name), None)
+        if driver_match is None:
+            raise Exception(f"Driver {name} could not be found!")
+        return driver_match
+
+    def __check_if_car_is_available(self, car_type):
+        for i in range(len(self.cars) - 1, 0, -1):
+            if type(self.cars[i]).__name__ == car_type and self.cars[i].is_taken is False:
+                return self.cars.pop(i)
+        else:
+            raise Exception(f"Car {car_type} could not be found!")
+
     def add_car_to_driver(self, driver_name: str, car_type: str):
-        pass
+        driver_object = self.__check_if_driver_exists(driver_name)
+        car_object = self.__check_if_car_is_available(car_type)
+
+        if driver_object.car:
+            old_model = driver_object.car.model
+            driver_object.car = car_object
+            car_object.is_taken = True
+            return f"Driver {driver_object.name} changed his car from {old_model} to {car_object.model}."
+
+        driver_object.car = car_object
+        car_object.is_taken = True
+        return f"Driver {driver_object.name} chose the car {car_object.model}."
+
 
     def add_driver_to_race(self, race_name: str, driver_name: str):
         pass
 
     def start_race(self, race_name: str):
         pass
-
