8b8e58499ac028826b85e407c484afb3276b00e3
aefremov2 <efremov.alex@yahoo.bg>
2024-04-25 08:35:34 +0300

    25.04.2024

--- OOP/exam_prep/11_dec_2021/unit_testing/test/test.py
+++ OOP/exam_prep/11_dec_2021/unit_testing/test/test.py
@@ -1,1 +1,17 @@
 from project.team import Team
+from unittest import TestCase, main
+
+
+class TestTeam(TestCase):
+    def setUp(self):
+        self.team = Team("Alex")
+
+    def test_correct_init(self):
+        self.assertEqual("Alex", self.team.name)
+        self.assertEqual({}, self.team.members)
+
+
+
+
+if __name__ == "__main__":
+    main()
