ef6829409a445f648d4f3603f7a6dcc3e00ee8fc
aefremov2 <efremov.alex@yahoo.bg>
2024-04-26 16:16:12 +0300

    26.04.2024

--- OOP/exam_prep/23_aug_2021/unit_testing/project/test/test.py
+++ OOP/exam_prep/23_aug_2021/unit_testing/project/test/test.py
@@ -17,6 +17,15 @@ class TestLibrary(TestCase):
         ex_res = "Name cannot be empty string!"
         self.assertEqual(ex_res, str(ex.exception))
 
+    def test_add_book_author_not_in_list_and_book_not_with_author(self):
+        self.library.add_book("Ewcia", "Poland")
+        self.assertEqual({"Ewcia": ["Poland"]}, self.library.books_by_authors)
+
+    def test_add_book_author_in_list_book_not_in_author(self):
+        self.library.add_book("Ewcia", "Poland")
+        self.library.add_book("Ewcia", "Bulgaria")
+
+        self.assertEqual({"Ewcia": ["Poland", "Bulgaria"]}, self.library.books_by_authors)
 
 if __name__ == "__main__":
     main()
