247e2ee0319565554a5f83b844359c9e89c02af4
aefremov2 <efremov.alex@yahoo.bg>
2024-04-26 10:20:19 +0300

    26.04.2024

---
+++ OOP/exam_prep/23_aug_2021/project/space_station.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from project.astronaut.astronaut_repository import AstronautRepository
+from project.planet.planet_repository import PlanetRepository
+
+
+class SpaceStation:
+    def __init__(self):
+        self.planet_repository: List[PlanetRepository] = []
+        self.astronaut_repository: List[AstronautRepository] = []
+
+    def add_astronaut(self, astronaut_type: str, name: str):
+        pass
+
+    def add_planet(self, name: str, items: str):
+        pass
+
+    def retire_astronaut(self, name: str):
+        pass
+
+    def recharge_oxygen(self):
+        pass
+
+    def send_on_mission(self, planet_name: str):
+        pass
+
+    def report(self):
+        pass
