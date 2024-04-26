530f6b043139b7f8f446b990eaba6eed86d6e54e
aefremov2 <efremov.alex@yahoo.bg>
2024-04-26 16:09:09 +0300

    26.04.2024

--- OOP/exam_prep/23_aug_2021/unit_testing/project/test/test.py
+++ OOP/exam_prep/23_aug_2021/unit_testing/project/test/test.py
@@ -1,1 +1,18 @@
 from project.library import Library
+from unittest import TestCase, main
+
+
+class TestLibrary(TestCase):
+    def setUp(self):
+        self.library = Library("Alex")
+
+    def test_correct_init(self):
+        self.assertEqual("Alex", self.library.name)
+        self.assertEqual({}, self.library.books_by_authors)
+        self.assertEqual({}, self.library.readers)
+
+
+
+
+if __name__ == "__main__":
+    main()
