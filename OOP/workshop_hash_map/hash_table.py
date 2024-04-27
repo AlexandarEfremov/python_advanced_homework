65f27ef2f4174675d81a231b142690b974e39b90
aefremov2 <efremov.alex@yahoo.bg>
2024-04-27 08:07:59 +0300

    27.04.2024

--- OOP/workshop_hash_map/hash_table.py
+++ OOP/workshop_hash_map/hash_table.py
@@ -2,5 +2,13 @@ class HashTable:
     def __init__(self):
         self.__keys = [None, None, None, None]
         self.__values = [None, None, None, None]
+        self.__length = 4
 
+    def __setitem__(self, key, value):
+        index = self.hash(key)
+        self.__keys[index] = key
+        self.__values[index] = value
+
+    def hash(self, key: str):
+        return sum([ord(el) for el in key]) % self.__length
 
